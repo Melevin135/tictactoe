7a5efeab0d46175b0e49151a23029a410764186a
Melvin Tachougaft <melvin.tachougaft@laplateforme.io>
2025-12-03 10:06:59 +0100

    int

---
+++ main.py
@@ -0,0 +1,64 @@
+import random
+
+plateau = [" " for _ in range(9)]
+
+#tableau
+def afficher_plateau():
+    print()
+    print(plateau[0], "|",plateau[1], "|",plateau[2])
+    print("--+---+--")
+    print(plateau[3], "|",plateau[4], "|",plateau[5])
+    print("--+---+--")
+    print(plateau[6], "|",plateau[7], "|",plateau[8])
+    print()
+
+#verifier la victoire
+def verifier(symbole):
+    lignes = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    for ligne in lignes:
+        if plateau[ligne[0]] == symbole and plateau[ligne[1]]== symbole and plateau[ligne[2]]==symbole:
+          return True
+    return False
+
+#tour du joueur
+def joueur():
+    choix=int(input("choisis une case (1 à 9) : "))
+    choix = choix -1
+    if plateau[choix] == " ":
+        plateau[choix] = "X"
+    else:
+        print("case occupée, rejoue.")
+        joueur()
+
+#tour du robot
+def robot():
+    print("le robot joue...")
+    choix = random.randint(0,8)
+    while plateau[choix] != " ":
+        print("salut")
+        choix = random.randint(0,8)
+    plateau[choix] = "O"
+
+#jeu pricipal
+def jouer():
+    afficher_plateau()
+    for tour in range(9):
+        if tour % 2 == 0:
+            joueur()
+        else:
+            robot()
+        afficher_plateau()
+        
+        if verifier("X"):
+            print("Victoire")
+            return
+        elif verifier("O"):
+            print("Défaite")   
+            return
+    print("égalité")    
+
+jouer()
